07773bebf8dbe34a4bcad96eee6710a3dc09b09d
geunyang <gaeballza@gmail.com>
2022-08-09 15:43:15 +0900

    220809

--- src/main/java/com/callor/todo/service/impl/UserServiceImplV1.java
+++ src/main/java/com/callor/todo/service/impl/UserServiceImplV1.java
@@ -1,17 +1,29 @@
 package com.callor.todo.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import com.callor.todo.model.AuthorityVO;
 import com.callor.todo.model.UserVO;
+import com.callor.todo.persistance.UserDao;
 import com.callor.todo.service.UserService;
 
 @Service("userServiceV1")
 public class UserServiceImplV1 implements UserService{
 	
-	
+	@Autowired
+	private UserDao userDao;
+	// 꼼수
+	@Bean
+	public void create_table() {
+		userDao.create_auth_table();
+		userDao.create_user_table();
+	}
+
 	@Override
 	public void create_user_table() {
 		// TODO Auto-generated method stub
@@ -26,23 +38,57 @@ public class UserServiceImplV1 implements UserService{
 
 	@Override
 	public List<AuthorityVO> select_auths(String username) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	public int role_insert(List<AuthorityVO> auths) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
 	public List<UserVO> selectAll() {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public UserVO findById(String id) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public int insert(UserVO vo) {
-		return 0;
 		
+		List<UserVO> userList = userDao.selectAll();
+		List<AuthorityVO> authList = new ArrayList<>();
+		
+		if(userList == null || userList.size() < 1) {
+			/*
+			 * 최초 회원 가입시 Enabled 를 true
+			 * ADMIN, USER 권한을 부여
+			 */
+			vo.setEnabled(true);
+			authList.add(AuthorityVO.builder()
+										.username(vo.getUsername())
+										.authority("ROLE_ADMIN")
+										.build());
+			authList.add(AuthorityVO.builder()
+					.username(vo.getUsername())
+					.authority("ROLE_USER")
+					.build());
+		} else {
+			authList.add(AuthorityVO.builder()
+					.username(vo.getUsername())
+					.authority("ROLE_USER")
+					.build());
+			
+		}
+		userDao.role_insert(authList);
+ 		return userDao.insert(vo);
 	}
 
 	@Override
@@ -53,12 +99,6 @@ public class UserServiceImplV1 implements UserService{
 
 	@Override
 	public int delete(String id) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int role_insert(List<AuthorityVO> auths) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
